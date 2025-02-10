39b14c0c4a45af43938c07297b7ee3265f8f7abe
A_Wolf1 <alexmerryman7@gmail.com>
2025-02-10 14:40:29 -0500

    Uncommented fish.printDetials

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		List<Fish> fishList = new ArrayList<>(); //creates empty new list to store values of all fish objects
 		fishList = CsvImporter.importCSV("resources/fish.csv"); // sets newly created fishList equal to output from the fish csv file
 		for (Fish fish: fishList) {
-			//fish.printDetails();
+			fish.printDetails();
 		}
 	}
 
