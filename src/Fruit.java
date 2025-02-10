8fe861b81cf90659882a48c6530cfafd5ca93fc1
danielle-esposito <dme0309@gmail.com>
2025-02-10 16:47:27 -0500

    barkedigt

--- src/Fruit.java
+++ src/Fruit.java
@@ -8,13 +8,10 @@ public class Fruit {
 	public Fruit(String[] fruit_values) {
 		this.fruit_name = fruit_values[0];
 		this.bell_value = Integer.parseInt(fruit_values[1]);
-		this.fruit_name = fruit_values[0];
-		this.bell_value = Integer.parseInt(fruit_values[1]);
 		this.capture_quote = fruit_values[2];
         this.ascii_art = fruit_values[3].replace("\\n", "\n"); // Fix newlines
-
-			
 	}
+    
 	public void printDetails() {
 		System.out.println(this.fruit_name + ", " + this.bell_value + ", " + this.capture_quote);
         System.out.println(this.ascii_art);
