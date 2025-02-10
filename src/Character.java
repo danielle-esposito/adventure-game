b952d4beadcebe4c28fdbfbb7357ffc02a73d55c
A_Wolf1 <alexmerryman7@gmail.com>
2025-02-10 17:24:15 -0500

    Completed printInventory class to Character class

--- src/Character.java
+++ src/Character.java
@@ -12,10 +12,37 @@ public class Character {
         this.name = name;
     }
 
-    public void setDebt(int debt) {
+    public void removeDebt(int debt) {
         this.debt+=debt;
     }
-    public void setMoney(int money) {
+    public void addMoney(int money) {
         this.money-=money;
+    }
+
+    public void printInventory(){
+        System.out.println("Fish");
+        if (fishInv != null){
+            for(Fish fish : fishInv){
+                fish.printDetails();
+            }
+        } else {
+            System.out.println("No Fish in inventory");
+        }
+        System.out.println("Bugs:");
+        if (bugInv != null){
+            for(Bug bug : bugInv){
+                bug.printDetails();
+            }
+        } else {
+            System.out.println("No Bugs in inventory");
+        }
+        System.out.println("Fruit: ");
+        if (fruitInv != null){
+            for(Fruit fruit : fruitInv){
+                fruit.printDetails();
+            }
+        } else {
+            System.out.println("No Fruit in inventory");
+        }
     }
 }
