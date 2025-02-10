e0a1c13e278fdce63718a66367b55d8d140e7118
A_Wolf1 <alexmerryman7@gmail.com>
2025-02-10 16:21:25 -0500

    Added refrence to future character class in Intro method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		Intro();
 
 		ArrayList<Fish> fishList = new ArrayList<>(); //creates empty new list to store values of all fish objects
 		fishList = CsvImporter.ImportFish("resources/fish.csv",fishList); // sets newly created fishList equal to output from the fish csv file
@@ -23,14 +23,33 @@ public class Main {
 		//	bug.printDetails();
 		//}
 
+		Intro();
+
+		boolean winCondition = false;
+
+		while(winCondition == false){
+			
+		}
 
 	}
 
 
 
 	public static void Intro(){
+		Scanner input = new Scanner(System.in);
+
 		IterativePrint.printString("Into back and forth narative to be written here");
-		System.out.println();
+		IterativePrint.printString("Nice to meet you! What's your name? ");
+
+		//String name = input.nextLine();
+		//Character player = new Character(name);
+		//Character defualt constuctor needs to be in here when prompted for name
+
+		input.close();
+	}
+
+	public static void Menu(){
+
 	}
 
 }
