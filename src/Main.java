2354bf1669a5aaf1c2e82b740b2b59fb68131653
A_Wolf1 <alexmerryman7@gmail.com>
2025-02-10 21:05:21 -0500

    Fixed fruit import

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,8 @@ public class Main {
 		fishList = CsvImporter.ImportFish("resources/fish.csv",fishList); // sets newly created fishList equal to output from the fish csv file
 		//ArrayList<Bug> bugList = new ArrayList<>();
 		//bugList = CsvImporter.ImportBugs("resources/bug.csv",bugList);
-		//ArrayList<Fruit> fruitList = new ArrayList<>();
-		//fruitList = CsvImporter.ImportFruit("resources/fruit.csv",fruitList);
+		ArrayList<Fruit> fruitList = new ArrayList<>();
+		fruitList = CsvImporter.ImportFruit("resources/fruit.csv",fruitList);
 
 		//Print details for all of the Items (Debug)
 		//for (Fish fish : fishList) {
@@ -47,7 +47,8 @@ public class Main {
 
 	public static void Menu(Character player){
 		IterativePrint.printString("You got some debt to pay off, here's what you can do!");
-		IterativePrint.printString("Current debt: ");
+		IterativePrint.printString("Current debt: " + player.debt);
+		IterativePrint.printString("Current money:" + player.money);
 		player.printInventory();
 
 	}
