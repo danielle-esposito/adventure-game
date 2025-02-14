3835e88c825a7b47896d7f14a8deb1a90d0b0302
A_Wolf1 <alexmerryman7@gmail.com>
2025-02-14 13:00:04 -0500

    added diologue

--- src/Shop.java
+++ src/Shop.java
@@ -5,9 +5,10 @@ public class Shop {
     public void visitShop(Character player) {
         Scanner input = new Scanner(System.in);
         IterativePrint.clearScreen();
+        IterativePrint.printString("Tommy : Welcome to Nook’s Cranny! I'm Tommy, and this is Timmy.", false);
+        IterativePrint.printString("Timmy: … Timmy!", false);
+        IterativePrint.printString("Tommy: Do you have anything to sell?");
         while (true) {
-            System.out.println("\n--- Welcome to the Shop! ---");
-            System.out.println("Your Money: " + player.money + " Bells");
             System.out.println("[1] Sell Fish");
             System.out.println("[2] Sell Bugs");
             System.out.println("[3] Sell Fruits");
@@ -26,11 +27,11 @@ public class Shop {
                     sellItems(player.fruitInv, player);
                     break;
                 case "4":
-                    IterativePrint.printString("Thanks for visiting!", true);
-                    //IterativePrint.clearScreen();
+                    IterativePrint.printString("Tommy: No problem! Feel free to come back anytime.", false);
+                    IterativePrint.printString("Timmy: ...anytime.", true);
                     return;
                 default:
-                    System.out.println("Invalid choice, try again.");
+                    IterativePrint.printString("Tommy: You can't sell those here, do you have anything else to sell?");
             }
         }
     }
@@ -38,7 +39,7 @@ public class Shop {
     private void sellItems(ArrayList<? extends Item> inventory, Character player) {
         IterativePrint.clearScreen();
         if (inventory.isEmpty()) {
-            System.out.println("You have no items to sell.");
+            System.out.println("Tommy: Looks like you have no items to sell, you can try another catagory!");
             return;
         }
 
@@ -50,5 +51,7 @@ public class Shop {
         player.money += totalEarned;
         inventory.clear();
         System.out.println("You sold your items for " + totalEarned + " Bells!");
+        IterativePrint.printString("Tommy: Thanks for your business!");
+        IterativePrint.printString("Timmy: ...business!", true);
     }
 }
