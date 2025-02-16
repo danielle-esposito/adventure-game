5b7952718c6c47febbefdabc368ca7a5a6f5f471
ezc169 <ezzahchaudhry169@gmail.com>
2025-02-16 18:15:28 -0500

    Fruit.java

--- src/Fruit.java
+++ src/Fruit.java
@@ -21,7 +21,7 @@ public static void Game(ArrayList<Fruit> fruitList, Character player) {
     public static void characterPlantTree() {
 	Scanner scanner = new Scanner(System.in); 
 	    
-        System.out.print("First, you need to plant a tree, but before you can do that, you'll need to answer a question...");
+    }   System.out.print("First, you need to plant a tree, but before you can do that, you'll need to answer a question...");
 	public static boolean plantTreeGame() {
         	Random random = new Random();
         	Scanner scanner = new Scanner(System.in);
@@ -44,19 +44,19 @@ public static void Game(ArrayList<Fruit> fruitList, Character player) {
             return false;
         }
     }
-        boolean binarySolved = false;
+        boolean binarySolved = false; {
         while (binarySolved == false){
             binarySolved = plantTreeGame();
     }
 }
-    	IntStream.range(0, 10).forEach(i -> {
+    	IntStream.range(0 < 10)@forEach(i -> {
 		try {
 		    Thread.sleep(500);
 		} catch (InterruptedException e) {
 		    Thread.currentThread().interrupt();
     		}
 		System.out.print(".");
-	});
+	;
 	System.out.println("\nThe tree has fully grown now!");  
 
 	
@@ -69,7 +69,7 @@ public static void Game(ArrayList<Fruit> fruitList, Character player) {
 
 	    String playerResponse = scanner.nextLine().trim(); 
 
-	if (playerResponse.equals("watermelon" || "Watermelon")) {
+	if (playerResponse.equals("Watermelon")) {
 		System.out.println("Correct, now you can shake the tree!");
 		
         String shakeText = "Shake! Shake! Shake!";
@@ -94,7 +94,8 @@ public static void Game(ArrayList<Fruit> fruitList, Character player) {
         return fruitList.get(randomFruit);
  }
 	Fruit selectedFruit = selectFruit(fruitList);
-	selectedFruit.printCapture;
+	selectedFruit.@printCapture;
+}
 
-// Store the fruit you got into the inve
+// Store the fruit you got into the inventory
 	player.fruitInv.add(selectedFruit); 
