12dcb0052fe5928c79cc7f1a809b7e62a764e36b
Danielle Esposito <dme0309@gmail.com>
2025-02-15 23:34:05 -0500

    I BARKED ON THE FISH!!!!

--- src/Fish.java
+++ src/Fish.java
@@ -11,27 +11,29 @@ public class Fish extends Item {
     }
     // section of code that handles execution of the game itself
     public static void Game(ArrayList<Fish> fishList, Character player) throws InterruptedException {
+        Scanner input = new Scanner(System.in); //defined scanner globally
         IterativePrint.clearScreen();
         
-        animateRiver.main();
+        animateRiver.main(input);
         IterativePrint.printString("Welcome to the river!", true);
         IterativePrint.printString("There are plenty of fish to catch!", true);
         IterativePrint.printString("Type 'cast' to cast your rod!");
         IterativePrint.printString("Type 'reel' to reel in your line! But be careful to not mistype!");
         IterativePrint.printString("Ready to fish? Type 'cast'!");
-        castAnimation.main();
-        Fish rewardFish = selectFish(fishList);
-        waitForFish.main(rewardFish);
+        castAnimation.main(input);
+        Fish rewardFish = selectFish(fishList); //selects a random fish from csv file and sets it equal to rewardFish
+        waitForFish.main(rewardFish); //runs the wait in water animation for a time determined by fish size
         System.out.println("You feel a fish tugging on the line!");
-        boolean catchStatus = reelFish.main(rewardFish);
+        boolean catchStatus = reelFish.main(rewardFish, input); //calls the reelFish class and returns a boolean if a fish was caught
         if (catchStatus == true) {
-            System.out.println(rewardFish.ascii_art);
-            System.out.println("Congrats! You caught a " + rewardFish.name);
+            rewardFish.printCapture();
             player.fishInv.addFirst(rewardFish);
         } else if (catchStatus == false) {
             System.out.println("Oh no! The fish got away!");
         }
-
+        System.out.println("Enter to continue");
+        input.nextLine();
+        input.close(); // closes scanner 
     }
 
     
@@ -57,7 +59,7 @@ public class Fish extends Item {
 }
 
 class animateRiver {
-    //class for painful little river/water/waves ascii animation, includes various methods for adjusting size/intensity/various aspects of the wave
+    // class for painful little river/water/waves ascii animation, includes various methods for adjusting size/intensity/various aspects of the wave
     static String[] river = {
     "~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ",
     "  ~    ~  ~    ~~   ~~    ~   ~~~    ~    ~~   ~  ~~   ~   ",
@@ -73,19 +75,19 @@ class animateRiver {
     "~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  ~  "
     };
     static boolean running = true;
-    public static void main() throws InterruptedException {
-        Scanner input = new Scanner(System.in);
-    
+    public static void main(Scanner input) throws InterruptedException {
         Random random = new Random();
         int direction =1;
         int shift =0;
             
+        // creates a new thread to wait for user input to stop the animation
         Thread inputThread = new Thread(() -> {
             input.nextLine();
             running=false;
         });
         inputThread.start();
-
+        
+        // loops the animation
         while (running) {
             IterativePrint.clearScreen();
             System.out.println("Enter to continue: ");
@@ -105,6 +107,7 @@ class animateRiver {
         }
     }
 }
+    // modifies the wave pattern to add randomness 
     public static String modifyWave(String line, Random random) {
         char[] chars = line.toCharArray();
         for (int i = 0; i < chars.length; i++) {
@@ -114,7 +117,7 @@ class animateRiver {
         }
         return new String(chars);
     }
-
+    // shifts the ascii back and forwarth
     public static String shiftLine(String line, int shift) {
         if (shift > 0) {
             return " ".repeat(shift) + line.substring(0, Math.max(0, line.length() - shift));
@@ -140,8 +143,7 @@ class castAnimation {
         "   \n         |         \\ \n         |          \\ \n         |           \\ \n         |            \\~ ~~   ~ ~~~~ ~\n~~~~~~~~~~~~~~~ \n ~   ~   ~ ~   ~~  ~~ ~    ~ ~   ~"
     };
     
-    public static void main() throws InterruptedException {
-        Scanner input = new Scanner(System.in);
+    public static void main(Scanner input) throws InterruptedException {
         if ((input.nextLine()).equals("cast")) {
             for (String frame : frames) {
                 IterativePrint.clearScreen();
@@ -155,9 +157,8 @@ class castAnimation {
         } else {
             IterativePrint.clearScreen();
             System.out.println("Type 'cast' to cast your rod!");
-            castAnimation.main();
+            castAnimation.main(input);
         }
-        input.close();
     }
 }
 
@@ -181,10 +182,11 @@ class waitForFish {
     };
         
     public static void main(Fish fish) throws InterruptedException {
+        // sets the time that the animation runs for 
         double duration = (fish.size * 1.5);
         double endTime = System.currentTimeMillis() + (duration * 500);
 
-
+        // loops the animation 
         while (System.currentTimeMillis() < endTime) {
             for (String frame : frames) {
                 IterativePrint.clearScreen();
@@ -200,41 +202,36 @@ class waitForFish {
 }
 
 class reelFish {
-    public static boolean main(Fish fish) {
-        Scanner input = new Scanner(System.in);
+    public static boolean main(Fish fish, Scanner input) {
         Random random = new Random();
 
+        // creates dynamic random bounds depending on the size of the fish
         double reelCountBound = fish.size * 1.5;
         int reelCountBoundInt = (int) reelCountBound;
 
         int reelCount = random.nextInt((reelCountBoundInt - fish.size) + 1) + fish.size;
         boolean catchStatus = true;
-        
+        // for loop for typing reel to reel in the fish
         System.out.println("Now! Type 'reel' to reel in your line!!");
         for(int i=0; i<=reelCount; i++) {
-            if (input.hasNextLine()){
-                String reel = input.nextLine();
-
-                if (reel.equals("reel") && (i < reelCount)) {
-                    System.out.println("Again!!");
-                    catchStatus=true;
-                } else if (reel.equals("reel") && (i == reelCount)){
-                    System.out.println("You caught the fish!");
-                    catchStatus = true;
-                    try {
-                        Thread.sleep(1500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    IterativePrint.clearScreen();
-                } else {
-                    System.out.println("It got away!");
-                    catchStatus = false;
-                    break;
+            String reel = input.nextLine();
+            if (reel.equals("reel") && (i < reelCount)) {
+                System.out.println("Again!!");
+            } else if (reel.equals("reel") && (i == reelCount)){
+                System.out.println("You caught the fish!");
+                catchStatus = true;
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+                IterativePrint.clearScreen();
+            } else {
+                System.out.println("It got away!");
+                catchStatus = false;
+                break;
             }
         }
-        input.close();
         return catchStatus;
     }
 }
