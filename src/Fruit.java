5d39452952b7e7cc39f510029765017785b168e2
A_Wolf1 <alexmerryman7@gmail.com>
2025-02-16 19:13:01 -0500

    Syntax errors fixed

--- src/Fruit.java
+++ src/Fruit.java
@@ -1,30 +1,49 @@
 import java.util.Scanner; 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.stream.IntStream; 
 
 public class Fruit extends Item{
 	
-	public static void Fruit(String[] values) {
+	public Fruit(String[] values) {
         super(values);
 	}
 
-public static void Game(ArrayList<Fruit> fruitList, Character player) {
+public static void Game(ArrayList<Fruit> fruitList, Character player, Scanner input) {
    IterativePrint.printString("Welcome to the Fruit Picking Game!");
    IterativePrint.printString("Here your task is to plant and then shake the trees and pick the fruit. This will be the fruit that you will then sell to the Shop to earn some more bells to pay off your debt!");
    
-   characterPlantTree();
-   characterShakeTree();
+   characterPlantTree(input);
+   for (int i = 0; i > 10; i++){
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            }
+        System.out.print(".");
+    }
+
+   characterShakeTree(input);
+
+    Fruit selectedFruit = selectFruit(fruitList);
+    selectedFruit.printCapture();
+    IterativePrint.printString("",true);
+
+    // Store the fruit you got into the inve
+        player.fruitInv.add(selectedFruit); 
    
    }
     
-    public static void characterPlantTree() {
-	Scanner scanner = new Scanner(System.in); 
-	    
+    public static void characterPlantTree(Scanner input) {
+
         System.out.print("First, you need to plant a tree, but before you can do that, you'll need to answer a question...");
-	public static boolean plantTreeGame() {
+
+        boolean binarySolved = false;
+        while (binarySolved == false){
+            binarySolved = plantTreeGame(input);
+        }
+    }
+	public static boolean plantTreeGame(Scanner input) {
         	Random random = new Random();
-        	Scanner scanner = new Scanner(System.in);
 
         	int decimalNumber = random.nextInt(256); // Generates a random number between 0 and 255
         	String binaryNumber = Integer.toBinaryString(decimalNumber);
@@ -32,9 +51,7 @@ public static void Game(ArrayList<Fruit> fruitList, Character player) {
         System.out.println("Convert the following decimal number to binary:");
         System.out.println(decimalNumber);
         System.out.print("Your answer: ");
-        String characterResponse = scanner.nextLine();
-		
-        scanner.close();
+        String characterResponse = input.nextLine();
  
 	if (characterResponse.equals(binaryNumber)) {
             System.out.println("That is correct! Your tree will now proceed to plant and grow!");
@@ -44,32 +61,16 @@ public static void Game(ArrayList<Fruit> fruitList, Character player) {
             return false;
         }
     }
-        boolean binarySolved = false;
-        while (binarySolved == false){
-            binarySolved = plantTreeGame();
-    }
-}
-    	IntStream.range(0, 10).forEach(i -> {
-		try {
-		    Thread.sleep(500);
-		} catch (InterruptedException e) {
-		    Thread.currentThread().interrupt();
-    		}
-		System.out.print(".");
-	});
-	System.out.println("\nThe tree has fully grown now!");  
 
 	
-    public static void characterShakeTree() {
-	System.out.println("Good, now that you've planted your tree, you'll need to shake it to retrieve the fruit.");
+    public static void characterShakeTree(Scanner input) {
+	System.out.println("Good, now that the tree has grown, you'll need to shake it to retrieve the fruit.");
 	    System.out.println("However, once again, you need to answer the following riddle...");
 	    System.out.println("You go at red, but stop at green. What am I?");
 
-	    Scanner scanner = new Scanner(System.in); 
+	    String playerResponse = input.nextLine().trim(); 
 
-	    String playerResponse = scanner.nextLine().trim(); 
-
-	if (playerResponse.equals("Watermelon")) {
+	if (playerResponse.equalsIgnoreCase("Watermelon")) {
 		System.out.println("Correct, now you can shake the tree!");
 		
         String shakeText = "Shake! Shake! Shake!";
@@ -83,7 +84,7 @@ public static void Game(ArrayList<Fruit> fruitList, Character player) {
             String padding = " ".repeat(i % 5);
             System.out.println(padding + shakeText);
         }
-        IterativePrint.printString("Good job! You've successfully made the fruit fall :)");
+        IterativePrint.printString("Good job! You've successfully made the fruit fall :)",true);
     } else {
 	    System.out.println("That is incorrect, try again!");
 	}
@@ -93,8 +94,4 @@ public static void Game(ArrayList<Fruit> fruitList, Character player) {
 	int randomFruit = random.nextInt(fruitList.size());
         return fruitList.get(randomFruit);
  }
-	Fruit selectedFruit = selectFruit(fruitList);
-	selectedFruit.printCapture;
-
-// Store the fruit you got into the inve
-	player.fruitInv.add(selectedFruit); 
+}
